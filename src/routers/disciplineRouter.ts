98b5a551db2261113b2dcff025b3465cf57a5282
a16we8r51fas6dfd <fernandoyudi.yaeda@gmail.com>
2022-05-01 21:29:04 -0300

    feat: create getDiscipline route

---
+++ src/routers/disciplineRouter.ts
@@ -0,0 +1,13 @@
+import { Router } from "express";
+import disciplineContoller from "../controllers/disciplineContoller.js";
+import { ensureAuthenticatedMiddleware } from "../middlewares/ensureAuthenticatedMiddleware.js";
+
+const disciplineRouter = Router();
+
+disciplineRouter.get(
+  "/disciplines",
+  ensureAuthenticatedMiddleware,
+  disciplineContoller.findMany
+);
+
+export default disciplineRouter;
